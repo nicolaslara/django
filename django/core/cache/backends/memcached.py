0ebe6b176a12fe0e2d7fb7d6eaf1070373ce519c
Nicolas Lara <nicolaslara@gmail.com>
2019-09-06 16:41:32 +0200

    naive implementation of an aio cache backend

--- django/core/cache/backends/memcached.py
+++ django/core/cache/backends/memcached.py
@@ -5,6 +5,7 @@ import re
 import time
 
 from django.core.cache.backends.base import DEFAULT_TIMEOUT, BaseCache
+from django.utils.asyncio import auto_async, async_unsafe
 from django.utils.functional import cached_property
 
 
@@ -62,33 +63,40 @@ class BaseMemcachedCache(BaseCache):
             timeout += int(time.time())
         return int(timeout)
 
+    @async_unsafe
     def add(self, key, value, timeout=DEFAULT_TIMEOUT, version=None):
         key = self.make_key(key, version=version)
         return self._cache.add(key, value, self.get_backend_timeout(timeout))
 
+    @auto_async
     def get(self, key, default=None, version=None):
         key = self.make_key(key, version=version)
         return self._cache.get(key, default)
 
+    @auto_async
     def set(self, key, value, timeout=DEFAULT_TIMEOUT, version=None):
         key = self.make_key(key, version=version)
         if not self._cache.set(key, value, self.get_backend_timeout(timeout)):
             # make sure the key doesn't keep its old value in case of failure to set (memcached's 1MB limit)
             self._cache.delete(key)
 
+    @async_unsafe
     def delete(self, key, version=None):
         key = self.make_key(key, version=version)
         self._cache.delete(key)
 
+    @async_unsafe
     def get_many(self, keys, version=None):
         key_map = {self.make_key(key, version=version): key for key in keys}
         ret = self._cache.get_multi(key_map.keys())
         return {key_map[k]: v for k, v in ret.items()}
 
+    @async_unsafe
     def close(self, **kwargs):
         # Many clients don't clean up connections properly.
         self._cache.disconnect_all()
 
+    @async_unsafe
     def incr(self, key, delta=1, version=None):
         key = self.make_key(key, version=version)
         # memcached doesn't support a negative delta
@@ -107,6 +115,7 @@ class BaseMemcachedCache(BaseCache):
             raise ValueError("Key '%s' not found" % key)
         return val
 
+    @async_unsafe
     def decr(self, key, delta=1, version=None):
         key = self.make_key(key, version=version)
         # memcached doesn't support a negative delta
@@ -125,6 +134,7 @@ class BaseMemcachedCache(BaseCache):
             raise ValueError("Key '%s' not found" % key)
         return val
 
+    @async_unsafe
     def set_many(self, data, timeout=DEFAULT_TIMEOUT, version=None):
         safe_data = {}
         original_keys = {}
@@ -135,9 +145,11 @@ class BaseMemcachedCache(BaseCache):
         failed_keys = self._cache.set_multi(safe_data, self.get_backend_timeout(timeout))
         return [original_keys[k] for k in failed_keys]
 
+    @async_unsafe
     def delete_many(self, keys, version=None):
         self._cache.delete_multi(self.make_key(key, version=version) for key in keys)
 
+    @async_unsafe
     def clear(self):
         self._cache.flush_all()
 
@@ -156,10 +168,12 @@ class MemcachedCache(BaseMemcachedCache):
             self._client = self._lib.Client(self._servers, **client_kwargs)
         return self._client
 
+    @async_unsafe
     def touch(self, key, timeout=DEFAULT_TIMEOUT, version=None):
         key = self.make_key(key, version=version)
         return self._cache.touch(key, self.get_backend_timeout(timeout)) != 0
 
+    @auto_async
     def get(self, key, default=None, version=None):
         key = self.make_key(key, version=version)
         val = self._cache.get(key)
@@ -191,3 +205,31 @@ class PyLibMCCache(BaseMemcachedCache):
         # libmemcached manages its own connections. Don't call disconnect_all()
         # as it resets the failover state and creates unnecessary reconnects.
         pass
+
+
+class AiomcacheCache(BaseMemcachedCache):
+    "An implementation of a cache binding using aiomcachecache"
+    def __init__(self, server, params):
+        import aiomcache
+        super().__init__(server, params, library=aiomcache, value_not_found_exception=ValueError)
+
+    @property
+    def _cache(self):
+        if getattr(self, '_client', None) is None:
+            host, port = self._servers[0].split(':')
+            self._client = self._lib.Client(host, port)
+        return self._client
+
+    async def get(self, key, default=None, version=None):
+        key = self.make_key(key, version=version).encode()
+        pickled = await self._cache.get(key)
+        if pickled is None:
+            return default
+        return pickle.loads(pickled)
+
+    async def set(self, key, value, timeout=DEFAULT_TIMEOUT, version=None):
+        key = self.make_key(key, version=version).encode()
+        pickled = pickle.dumps(value, pickle.HIGHEST_PROTOCOL)
+        if not await self._cache.set(key, pickled, self.get_backend_timeout(timeout)):
+            # make sure the key doesn't keep its old value in case of failure to set (memcached's 1MB limit)
+            await self._cache.delete(key)
